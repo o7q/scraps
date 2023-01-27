1ea457ef6bb2cd4b8ee178c56044b74b5873024c
o7q <jev.o7q@gmail.com>
2023-01-27 12:59:02 -0600

    Update `NumberSheet`

--- projects/NumberSheet/src/NumberSheet/main.cpp
+++ projects/NumberSheet/src/NumberSheet/main.cpp
@@ -15,7 +15,9 @@ int main()
         string INPUT;
         cin >> INPUT;
 
-        string FINAL = "";
+        string HEAD = "";
+        string CURRENT = "";
+        bool doHash = true;
 
         stringstream master_stream(INPUT);
         string master_seg;
@@ -35,12 +37,14 @@ int main()
                 {
                     for (int j = stoi(num_seglist[0]); j <= stoi(num_seglist[1]); j++)
                     {
-                        if (j == stoi(num_seglist[1])) FINAL += to_string(j) + ")";
-                        else FINAL += to_string(j) + ")\n\n\n\n";
+                        if (j == stoi(num_seglist[1])) CURRENT += to_string(j) + ")";
+                        else CURRENT += to_string(j) + ")\n\n\n\n";
                     }
                 }
 
-                if (master_seglist.size() > 1 && i + 1 != master_seglist.size()) FINAL += "\n\n&\n\n";
+                if (master_seglist.size() > 1 && i + 1 != master_seglist.size()) CURRENT += "\n\n&\n\n";
+
+                HEAD += (doHash == true ? "#" : "") + num_seglist[0] + "-" + num_seglist[1] + ", ";
             }
 
             if (master_seglist[i].find('=') < master_seglist[i].length())
@@ -54,15 +58,23 @@ int main()
                 {
                     for (int j = stoi(num_seglist[0]); j <= stoi(num_seglist[1]); j += 2)
                     {
-                        if (j == stoi(num_seglist[1])) FINAL += to_string(j) + ")";
-                        else FINAL += to_string(j) + ")\n\n*\n\n";
+                        if (j == stoi(num_seglist[1])) CURRENT += to_string(j) + ")";
+                        else CURRENT += to_string(j) + ")\n\n*\n\n";
                     }
                 }
 
-                if (stoi(num_seglist[0]) % 2 != 0 && stoi(num_seglist[1]) % 2 == 0) FINAL = FINAL.substr(0, FINAL.size() - 5);
-                if (master_seglist.size() > 1 && i + 1 != master_seglist.size()) FINAL += "\n\n&\n\n";
+                if (stoi(num_seglist[0]) % 2 != 0 && stoi(num_seglist[1]) % 2 == 0) CURRENT = CURRENT.substr(0, CURRENT.size() - 5);
+                if (master_seglist.size() > 1 && i + 1 != master_seglist.size()) CURRENT += "\n\n&\n\n";
+
+                string type = stoi(num_seglist[0]) % 2 == 0 ? "even" : "odd";
+                HEAD += (doHash == true ? "#" : "") + num_seglist[0] + "-" + num_seglist[1] + " " + type + ", ";
             }
+
+            doHash = false;
         }
+
+        HEAD = HEAD.substr(0, HEAD.size() - 2);
+        string FINAL = HEAD + "\n\n" + CURRENT;
 
         ofstream out;
         out.open("sheet" + to_string(sheet_index) + ".txt");
